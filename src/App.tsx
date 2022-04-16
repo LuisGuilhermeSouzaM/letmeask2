46af06d563b05d5b54b052f6671d91aaaa381d5f
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-04-16 13:50:42 -0300

    nao mais pagina branca

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-// import { Home } from "./pages/Home";
+import {Routes, Route} from "react-router-dom";
+
+import { Home } from "./pages/Home";
 import {NewRoom} from "./pages/NewRoom";
 
 function App() {
   return (
-    <div>
-      <NewRoom />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rooms/new" element={<NewRoom />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+// import your route components too
 
-import { firebase, auth, database } from './services/firebase';
 
-import './styles/global.scss';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/rooms/new" element={<App />} />
+          <Route index element={<NewRoom />} />
+        </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
